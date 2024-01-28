329aa6567720b718866ab7a3c26714a1e5c7712c
Nam <doanngocnam3002@gmail.com>
2024-01-28 07:15:06 +0700

    multiple select componet done

--- note/src/App.tsx
+++ note/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css'
 import { Routes, Route, Navigate} from 'react-router-dom'
 import NewNote from './components/NewNote'
--- select/src/App.tsx
+++ select/src/App.tsx
@@ -1,34 +1,101 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { XMarkIcon, TrashIcon } from '@heroicons/react/16/solid'
+import {v4 as uuidV4} from 'uuid'
+
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type Tag={
+  id: string
+  value: string
+}
+
+const initTags = [
+  {
+    id: '1',
+    value: 'JavaScript'
+  },
+  {
+    id: '2',
+    value: 'Java'
+  },
+  {
+    id: '3',
+    value: 'An Chee'
+  }
+]
+
+const Item=({value, remove}:{value:string, remove:(value:string)=>void})=>{
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-2/12 h-8 border-green_2 border rounded-full p-2 shadow-none mx-1 flex flex-row items-center'>
+
+      <div onClick={()=>remove(value)}>
+        <XMarkIcon className='w-4 h-4 fill-airbnb_rausch cursor-pointer' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <p className='text-xs'>{value}</p>
+    </div>
+  )
+}
+
+function App() {
+  const [query, setQuery] = useState<string>('')
+  const [tags, setTags]=useState<Tag[]>(initTags)
+  const [selectedTags, setSelectedTags]=useState<Tag[]>([])
+  const [isOpen, setIsOpen]=useState<boolean>(false)
+
+  const handleSelect=(tagx:Tag):void=>{
+    if(selectedTags.includes(tagx)) return
+    setSelectedTags(prev=>[...prev, tagx])
+  }
+
+  const handleRemove=(value:string)=>{
+    setSelectedTags(prev=>prev.filter(tag=>tag.value !== value))
+  }
+
+  const handleDelete=(value:string)=>{
+    setTags(prev=>prev.filter(tag=>tag.value !== value))
+  }
+
+  const handleNewTag=(e:React.FormEvent)=>{
+    e.preventDefault()
+    if(query === '') return
+    const newTag={value: query, id: uuidV4()}
+    setTags(prev=>[...prev, newTag])
+    setSelectedTags(prev=>[...prev, newTag])
+    setQuery('')
+  }
+  return (
+    <div className='bg-city_light h-full w-full flex flex-row items-center justify-center'>
+      <div className='w-1/3  flex flex-col items-center p-8 bg-white shadow-st rounded .relative'>
+        <div className='w-full min-h-11 h-max rounded p-2 mb-4 flex flex-row flex-wrap shadow-st'>
+          {selectedTags.map((stag)=>(<Item value={stag.value} key={stag.id} remove={handleRemove}/>))}
+        </div>
+        <form className='w-full' onSubmit={(e)=>handleNewTag(e)}>
+          <input type="text" 
+            id='tag'
+            name='tag'
+            value={query} 
+            onChange={(e)=>setQuery(e.target.value)} 
+            className='w-full h-8 p-4 border border-green_2 rounded-full outline-none'
+            onFocus={()=>setIsOpen(true)} />
+        </form>
+
+        <div className='w-full max-h-32 overflow-y-scroll flex flex-col .absolute top-3/4 shadow-st mt-2'>
+          {isOpen && tags.filter((tag)=>tag.value.includes(query.toLowerCase())).map((tagx)=>(
+            <div className='w-full h-10 p-2 flex flex-row item justify-between cursor-pointer' key={tagx.id} >
+              <div  onClick={()=>handleSelect(tagx)}>
+                {tagx.value}
+              </div>
+              <div onClick={()=>handleDelete(tagx.value)}>
+                <TrashIcon className='w-4 h-4 fill-airbnb_rausch cursor-pointer' />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
